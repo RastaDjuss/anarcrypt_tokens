97201a8b335925a5c1d903a0930ec5879e5c849d
rastadjuss <folienesie@gmail.com>
2024-11-21 23:50:10 -0500

    remolved anarcrypt-tokens

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-use spl_token; // Ensure this is declared properly
-
-fn main() {
-    // Correct usage depending on whether it's a function or macro
-    let result = spl_token::metadata("AwvWXhPjCoGz39HPYJ6GR7huC3Hc1vHyZ4LuiDdHXcVa");
-    // Handle the result as needed
-}
