7acee779ba3f0ec35f4507df972c82959f01c565
Anarchsun <folienesie@gmail.com>
2025-03-01 04:37:59 -0500

    Delete tokens/Chaotic-fractal/r=3.57-pinata directory

--- tokens/Chaotic-fractal/r=3.57-pinata/endpoint.js
+++
@@ -1,7 +0,0 @@
-const { Connection } = require('@solana/web3.js');
-
-// Configurez l'URL provenant de QuickNode
-const QUICKNODE_RPC_URL = "https://<some_endpoint>.solana-mainnet.quiknode.pro/<your_api_key>/";
-
-// Créez une connexion avec Solana
-const connection = new Connection(QUICKNODE_RPC_URL, 'confirmed');
